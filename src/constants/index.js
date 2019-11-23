a70f6756c3234f79ba0f7e12e8477bc21e528301
hobroker <igor.leahu24@gmail.com>
2019-11-23 21:18:38 +0200

    change constant

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,6 @@
-export const CMD_NAME = 'ghcd';
+import { bin } from '../../package';
+
+export const CMD_NAME = Object.keys(bin).pop() || 'ghcd';
 export const REPOSITORY = 'https://github.com/hobroker/gh-docker-cd';
 
 export const DEPLOYMENT_STATUSES = {
